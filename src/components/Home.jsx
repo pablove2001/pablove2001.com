a8a4ebf8689ae7a44156596faf766fda58df81bd
Pablo Vergara <vergarapablo2001@gmail.com>
2022-09-30 17:14:17 -0500

    changed bg-home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,14 @@ const Home = () => {
         className="min-h-screen bg-fixed flex items-center"
         style={{ backgroundImage: `url(${bgHome})` }}
       >
-        <div className="max-w-7xl mx-auto px-5 py-20 h-auto">
+        <div className="max-w-7xl mx-auto p-5 h-auto backdrop-blur-sm rounded-3xl bg-black/60">
           <div className="md:flex items-center">
             <img
               src={profile}
               alt="fondo"
               className="md:w-64 md:h-64 w-72 h-72 rounded-full border-2 border-white m-auto"
             />
-            <div className="md:w-auto md:ml-6 md:mt-0 mt-10 h-auto text-white md:text-5xl text-4xl text-center">
+            <div className="md:w-auto md:ml-6 md:mt-0 mt-3 h-auto text-white md:text-5xl text-4xl text-center">
               <p>
                 Hi!
                 <br />
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
   ];
   return (
     <>
-      <nav className="fixed top-0 bg-black sm:backdrop-filter sm:backdrop-blur-lg sm:bg-opacity-80 bg-opacity-90 w-full z-10">
+      <nav className="fixed top-0 bg-black sm:backdrop-blur-lg sm:bg-opacity-80 bg-opacity-90 w-full z-10">
         <div className="max-w-7xl mx-auto px-5">
           <div className="flex items-center justify-between h-16">
             <a href="#home" className="sm:hover:text-gray-500 duration-300">
